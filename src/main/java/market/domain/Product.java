121f0d768fea764ceacbb93d7a06cdc95eec6ae2
sting2804 <slinnik284@gmail.com>
2015-09-26 17:06:33 +0300

    project initialization

---
+++ src/main/java/market/domain/Product.java
@@ -0,0 +1,15 @@
+package market.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+
+
+
+}
