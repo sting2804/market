0950b980532a4094d1af13af54887eaab57d29c7
sting2804 <slinnik284@gmail.com>
2015-11-05 22:18:47 +0200

    захардкодил пользователя user с ролью USER, добавил фильтр для правильной работы CORS, добавил два спринговых плагина вроде как для работы с сессиями

--- src/main/java/com/bdls/market/MarketApplication.java
+++ src/main/java/com/bdls/market/MarketApplication.java
@@ -3,6 +3,7 @@ package com.bdls.market;
 import com.bdls.market.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class MarketApplication {
---
+++ src/main/java/com/bdls/market/filter/SimpleCORSFilter.java
@@ -0,0 +1,34 @@
+package com.bdls.market.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
